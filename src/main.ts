2905b33228c948e77a20e4e621899bb92f3e06ea
joaquimlp <joaquimlp2013@gmail.com>
2025-09-07 17:12:11 -0300

    FIx

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { });
+
+  const configService = app.get<ConfigService<Env>>(ConfigService);
+  const port = configService.get('PORT');
+  console.log(`Listening on port ${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
